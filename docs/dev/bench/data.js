b2d899d2593e5ecf565ea4355855d0f95d3339d9
github-action-benchmark <github@users.noreply.github.com>
2023-06-22 17:20:28 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for 0eaa449b78d35e94cc61261a0b7b08534f965997

---
+++ docs/dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1687454428496,
+  "repoUrl": "https://github.com/mwear/opentelemetry-collector-contrib",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "matthew.wear@gmail.com",
+            "name": "Matthew Wear",
+            "username": "mwear"
+          },
+          "committer": {
+            "email": "matthew.wear@gmail.com",
+            "name": "Matthew Wear",
+            "username": "mwear"
+          },
+          "distinct": true,
+          "id": "0eaa449b78d35e94cc61261a0b7b08534f965997",
+          "message": "setup perf action",
+          "timestamp": "2023-06-22T10:09:56-07:00",
+          "tree_id": "f94cca9223e1a1d1ab4cfb2964eb0cda351107f5",
+          "url": "https://github.com/mwear/opentelemetry-collector-contrib/commit/0eaa449b78d35e94cc61261a0b7b08534f965997"
+        },
+        "date": 1687454391266,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "My Custom Smaller Is Better Benchmark - CPU Load",
+            "value": 50,
+            "unit": "Percent",
+            "range": "5%",
+            "extra": "My Optional Information for the tooltip"
+          },
+          {
+            "name": "My Custom Smaller Is Better Benchmark - Memory Used",
+            "value": 100,
+            "unit": "Megabytes"
+          }
+        ]
+      }
+    ]
+  }
+}
