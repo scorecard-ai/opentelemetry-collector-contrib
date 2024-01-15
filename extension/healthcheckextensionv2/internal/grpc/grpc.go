7c37fe450c1fb206d5af0491d64c5f38f64e7943
Matthew Wear <matthew.wear@gmail.com>
2024-01-15 14:46:11 -0800

    Add grpc tests

--- extension/healthcheckextensionv2/internal/grpc/grpc.go
+++ extension/healthcheckextensionv2/internal/grpc/grpc.go
@@ -11,17 +11,6 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-var statusToServingStatusMap = map[component.Status]healthpb.HealthCheckResponse_ServingStatus{
-	component.StatusNone:             healthpb.HealthCheckResponse_NOT_SERVING,
-	component.StatusStarting:         healthpb.HealthCheckResponse_NOT_SERVING,
-	component.StatusOK:               healthpb.HealthCheckResponse_SERVING,
-	component.StatusRecoverableError: healthpb.HealthCheckResponse_SERVING,
-	component.StatusPermanentError:   healthpb.HealthCheckResponse_NOT_SERVING,
-	component.StatusFatalError:       healthpb.HealthCheckResponse_NOT_SERVING,
-	component.StatusStopping:         healthpb.HealthCheckResponse_NOT_SERVING,
-	component.StatusStopped:          healthpb.HealthCheckResponse_NOT_SERVING,
-}
-
 func (s *Server) Check(ctx context.Context, req *healthpb.HealthCheckRequest) (*healthpb.HealthCheckResponse, error) {
 	var err error
 	var ev *component.StatusEvent
@@ -108,6 +97,17 @@ func (s *Server) Watch(req *healthpb.HealthCheckRequest, stream healthpb.Health_
 			return status.Error(codes.Canceled, "Stream has ended.")
 		}
 	}
+}
+
+var statusToServingStatusMap = map[component.Status]healthpb.HealthCheckResponse_ServingStatus{
+	component.StatusNone:             healthpb.HealthCheckResponse_NOT_SERVING,
+	component.StatusStarting:         healthpb.HealthCheckResponse_NOT_SERVING,
+	component.StatusOK:               healthpb.HealthCheckResponse_SERVING,
+	component.StatusRecoverableError: healthpb.HealthCheckResponse_SERVING,
+	component.StatusPermanentError:   healthpb.HealthCheckResponse_NOT_SERVING,
+	component.StatusFatalError:       healthpb.HealthCheckResponse_NOT_SERVING,
+	component.StatusStopping:         healthpb.HealthCheckResponse_NOT_SERVING,
+	component.StatusStopped:          healthpb.HealthCheckResponse_NOT_SERVING,
 }
 
 func (s *Server) toServingStatus(ev *component.StatusEvent) healthpb.HealthCheckResponse_ServingStatus {
